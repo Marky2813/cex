5c4f9e24264af21ab3dc58f22a1a095abdb04cbc
Sarthak <messagetosarthak@gmail.com>
2026-05-16 10:56:29 +0530

    created orderbook and balances in-memory databases

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,59 @@
-import express from "express"; 
+import express from "express";
+import { Heap } from 'heap-js';
 
-const app = express(); 
+const app = express();
 
-const BALANCES = {
-
+type AssetBalance = {
+  total: number,
+  locked: number
 }
 
-const ORDERBOOKS = {
-
+type UserBalance = {
+  [symbol: string]: AssetBalance
 }
 
-app.post("signup", (req,res) => {
+type Balances = {
+  [userId: string]: UserBalance
+}
+
+const BALANCES: Balances = {
+}
+
+type OrderStatus = "pending" | "partial";
+
+type Order = {
+  orderId: string,
+  userId: string,
+  orderStatus: OrderStatus,
+  qty: number,
+  amount: number
+}
+type Node = {
+  price: number,
+  orders: Order[]
+}
+const customPriorityComparatorMin = (a: Node, b: Node) => a.price - b.price;
+const minHeap = new Heap(customPriorityComparatorMin);
+const minMap = new Map<number, Order[]>();
+
+const customPriorityComparatorMax = (a: Node, b: Node) => b.price - a.price;
+const maxHeap = new Heap(customPriorityComparatorMax);
+const maxMap = new Map<number, Order[]>();
+
+
+const orderBooks = {
+  sell: {
+    minHeap, minMap
+  },
+  buy: {
+    maxHeap, maxMap
+  }
+}
+
+app.post("signup", (req, res) => {
 
 })
 
-app.post("signin", (req,res) => {
-  
+app.post("signin", (req, res) => {
+
 })
